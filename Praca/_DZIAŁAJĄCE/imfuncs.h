7be2a3d16d9e1cbb7f045123cf371e0849327b76
matplat <matplatekk@gmail.com>
2017-05-30 17:02:21 +0200

    commit 1 płatek

---
+++ Praca/_DZIAŁAJĄCE/imfuncs.h
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+void insertionSort(int tablica[]);
+int sharpen(Mat image);
+int blur(Mat image);
+int contrast_change (Mat image);
