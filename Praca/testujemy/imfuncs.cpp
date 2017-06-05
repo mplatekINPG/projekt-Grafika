bdaefcd6f518c8d2b73f8e7498113fb93bcffc1f
matplat <matplatekk@gmail.com>
2017-06-05 21:30:49 +0200

    Mikołaj, testuj

---
+++ Praca/testujemy/imfuncs.cpp
@@ -0,0 +1,116 @@
+#include <cstdlib>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+Mat ImageLoad ()
+{
+	OPENFILENAME ofn;
+    char szFileName[MAX_PATH] = "";
+
+    ZeroMemory(&ofn, sizeof(ofn));
+
+    ofn.lStructSize = sizeof(ofn);
+    ofn.hwndOwner = NULL;
+    ofn.lpstrFilter = "bmp Files (*.bmp)\0*.bmp\0All Files (*.*)\0*.*\0";
+    ofn.lpstrFile = szFileName;
+    ofn.nMaxFile = MAX_PATH;
+    ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+    ofn.lpstrDefExt = "bmp";
+
+
+	if(GetOpenFileName(&ofn))
+    {
+		Mat image;
+		image = imread(szFileName, CV_LOAD_IMAGE_COLOR);
+		if ( !image.data )
+		{cout<<"BLAD WCZYTYWANIA";}
+		namedWindow("Color Image", WINDOW_AUTOSIZE);
+  		imshow("Color Image", image);
+ 		return image;
+	}
+}
+
+//void ImageSave (Mat * image);
+/*{
+	cout<<"Podaj nazwe nowego pliku\n";
+	char *name = new char[50];
+	cin>>name;
+	cvSaveImage(name, *image);
+	delete [] name;
+	return;
+}*/
+
+void insertionSort(int tablica[])
+{
+    int temp, i , j;
+    for(i = 0; i < 9; i++){
+        temp = tablica[i];
+        for(j = i-1; j >= 0 && temp < tablica[j]; j--){
+            tablica[j+1] = tablica[j];
+        }
+        tablica[j+1] = temp;
+    }
+}
+int sharpen(Mat image)
+{
+	Mat kernel(3,3,CV_32F,cv::Scalar(0));	//rozmiar macierzy teorzacej baze filtra
+					//nalezy teraz ustaliæ wartoœci macierzy dla tworzonego filtra
+	kernel.at<double>(1,1)= 5.0;
+	kernel.at<double>(0,1)= -1.0;
+	kernel.at<double>(2,1)= -1.0;
+	kernel.at<double>(1,0)= -1.0;
+	kernel.at<double>(1,2)= -1.0;
+
+			//filtrowanie obrazu
+	cv::filter2D(image,image,image.depth(),kernel);
+
+	namedWindow( "Sharpen5", WINDOW_AUTOSIZE );
+  imshow( "Sharpen5", image );
+  waitKey(0);
+  return 0;
+}
+int blur(Mat image)
+{
+    Mat new_image = Mat::zeros( Image.size(), Image.type() );
+
+    blur( Image, new_image, Size( 5, 5 ), Point(-1,-1) );
+    imshow("Rozmycie", new_image);
+    waitKey(0);
+    return 0;
+}
+
+int contrast_change (Mat image, double change)
+{
+	Mat new_image = Mat::zeros( image.size(), image.type() );
+	image.convertTo(new_image, -1, change, 0);
+    imshow("final", new_image);
+	return 0;
+}
+int brigthness_change(Mat image, double change)
+{
+	Mat new_image = Mat::zeros( image.size(), image.type() );
+	image.convertTo(new_image, -1, 1, change);
+    imshow("final", new_image);
+	return 0;
+}
+
+int sepia (Mat image)
+{
+Mat_<float> sepia(3,3);
+
+sepia << .131,.534,.272    //wartoœci najczesciej uzywane w uzyskiwaniu sepii
+        ,.168,.686,.349
+        ,.189,.769,.393;
+Mat out;
+cv::transform(image,out,sepia);
+imshow("i",out);
+waitKey();
+  return 0;
+}
+
