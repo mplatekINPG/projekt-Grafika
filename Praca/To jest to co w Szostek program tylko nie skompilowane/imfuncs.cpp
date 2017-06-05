43552fac867153e7b37f82a54c8ea630386f127c
MrowkaMINPG <slawomirautomatyczny@gmail.com>
2017-06-05 15:59:37 +0200

    Mrówka Commit #7

--- Praca/To jest to co w Szostek program tylko nie skompilowane/imfuncs.cpp
+++ Praca/To jest to co w Szostek program tylko nie skompilowane/imfuncs.cpp
@@ -12,7 +12,7 @@ Mat ImageLoad ()
 {
 	Mat image;
 	image = imread("lena.bmp", CV_LOAD_IMAGE_COLOR);
-	if ( !image.data ) 
+	if ( !image.data )
 	{cout<<"BLAD WCZYTYWANIA";}
 	namedWindow("Color Image", WINDOW_AUTOSIZE);
     imshow("Color Image", image);
@@ -43,66 +43,29 @@ void insertionSort(int tablica[])
 int sharpen()
 {
 	Mat image = ImageLoad();
-	Mat kernel(3,3,CV_32F,cv::Scalar(0));	//rozmiar macierzy teorzacej baze filtra
-					//nalezy teraz ustaliæ wartoœci macierzy dla tworzonego filtra
-	kernel.at<double>(1,1)= 5.0;
-	kernel.at<double>(0,1)= -1.0;
-	kernel.at<double>(2,1)= -1.0;
-	kernel.at<double>(1,0)= -1.0;
-	kernel.at<double>(1,2)= -1.0;
-
-			//filtrowanie obrazu
-	cv::filter2D(image,image,image.depth(),kernel);
-
-	namedWindow( "Sharpen5", WINDOW_AUTOSIZE );
-  imshow( "Sharpen5", image );
-  waitKey(0);
-  return 0;
+    Mat new_image = Mat::zeros( Image.size(), Image.type() );
+    namedWindow("Wyostrzenie", WINDOW_AUTOSIZE );
+    cv::addWeighted(image, 1.5, new_image, -0.5, 0, image);
+    imshow("Wyostrzenie", new_image);
+    waitKey(0);
+    return 0;
 }
 
 int blur()
 {
-	Mat image = ImageLoad();
-    Mat next;
-    if( !image.data )
-    {
-        return -1;
-    }
-    //stworzenie tablicy 3x4 do "jezdzenia po obrazku"
-    int tablica[9];
-    next = image.clone();                     //przekopiowanie tablicy do drugiej
-    for(int y = 0; y < image.rows; y++)
-        for(int x = 0; x < image.cols; x++)
-            next.at<uchar>(y,x) = 0.0;
-
-        for(int y = 1; y < image.rows - 1; y++){
-            for(int x = 1; x < image.cols - 1; x++){
-                tablica[0] = image.at<uchar>(y - 1 ,x - 1);
-                tablica[1] = image.at<uchar>(y, x - 1);
-                tablica[2] = image.at<uchar>(y + 1, x - 1);
-                tablica[3] = image.at<uchar>(y - 1, x);
-                tablica[4] = image.at<uchar>(y, x);
-                tablica[5] = image.at<uchar>(y + 1, x);
-                tablica[6] = image.at<uchar>(y - 1, x + 1);
-                tablica[7] = image.at<uchar>(y, x + 1);
-                tablica[8] = image.at<uchar>(y + 1, x + 1);
-                insertionSort(tablica);
-                // przypisanie mediany do wyorodkowanego elementu tablicy
-                next.at<uchar>(y,x) = tablica[4];
-            }
-        }
-        //wyowietlenie obu obrazków
-        namedWindow("final");
-        imshow("final", next);
-        namedWindow("initial");
-        imshow("initial", image);
-        return 0;
+	Mat Image = ImageLoad();
+    Mat new_image = Mat::zeros( Image.size(), Image.type() );
+    namedWindow("Rozmycie", WINDOW_AUTOSIZE );
+    blur( Image, new_image, Size( 5, 5 ), Point(-1,-1) );
+    imshow("Rozmycie", new_image);
+    waitKey(0);
+    return 0;
 }
 
 int contrast_change (double change)
 {
 	Mat image = ImageLoad();
-	Mat new_image = Mat::zeros( image.size(), image.type() ); 
+	Mat new_image = Mat::zeros( image.size(), image.type() );
 	image.convertTo(new_image, -1, change, 0);
     imshow("final", new_image);
 	return 0;
@@ -110,16 +73,16 @@ int contrast_change (double change)
 int brigthness_change(double change)
 {
 	Mat image = ImageLoad();
-	Mat new_image = Mat::zeros( image.size(), image.type() ); 
+	Mat new_image = Mat::zeros( image.size(), image.type() );
 	image.convertTo(new_image, -1, 1, change);
     imshow("final", new_image);
-	return 0;	
+	return 0;
 }
 
 int sepia ()
 {
 Mat img = ImageLoad();
-Mat_<float> sepia(3,3); 
+Mat_<float> sepia(3,3);
 
 sepia << .131,.534,.272    //wartoœci najczesciej uzywane w uzyskiwaniu sepii
         ,.168,.686,.349
